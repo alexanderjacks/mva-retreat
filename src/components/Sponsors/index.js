130c00aa693f966434f1afb841c40fe5b7e3c6a4
alexanderjacks <alexanderthejacks@gmail.com>
2019-08-06 21:46:50 -0700

    :memo: more info added

--- src/components/Sponsors/index.js
+++ src/components/Sponsors/index.js
@@ -12,22 +12,39 @@ function Sponsors() {
     <Container>
     <div class="top-clearance-xl bottom-clearance">
         <div class="centermax">
-          	<H2><span class="red-text">
+          	<H2>
+              <span class="red-text">
           		Sponsors
           		</span>
           	</H2>
         </div>
         <div class="centermax">
           	<div class="keep-50w">
-          		<TMobileLogo />
-	          	<P class="centermax"><span class="red-text">
+	          	<P class="centermax">
+          		  <TMobileLogo />
+                <span class="red-text">
 	            	Founding Sponsor
 	            	</span>
 	          	</P>
-              <ButtonOut href="http://minorityvets.org/sponsorelr/" target="_blank" rel="noopener" >
-                Become a Sponsor
-              </ButtonOut>
           </div>
+        </div>
+        <div class="centermax">
+            <div class="keep-50w">
+              <P class="centermax">
+                <H2 class="red-text">
+                  <span class="red-text">
+                    Individual Donors
+                  </span>
+                </H2>
+                <p class="red-text">Robert White</p>
+                <p class="red-text">Bill Kirst</p>
+              </P>
+          </div>
+        </div>
+        <div class="centermax">
+          <ButtonOut href="http://minorityvets.org/sponsorelr/" target="_blank" rel="noopener" >
+            Become a Sponsor
+          </ButtonOut>
         </div>
     </div>
     </Container>
